e34134ff3b8e98d0e5ab68f1f4f199dba8e50f81
Gaye-ndeye <ngaye6881@gmail.com>
2025-03-23 21:47:05 +0000

    tailwindcss

---
+++ tailwindconfig.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          bleu: '#2563eb',
+          orange:'#fbbf24',
+          rouge:'#ef4444',
+          blue:'#0c4a6e',
+          green:'#e2e8f0'
+         
+        },
+      }
+    }
+  }
